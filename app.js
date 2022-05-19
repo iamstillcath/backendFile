1177ea025a88791ae289e4e9e7835eb2aade2006
iamstillcath <catherine2lucia@gmail.com>
2022-05-19 23:32:02 +0100

    deploy fixes

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ app.use((req, res, next) => {
 //   res.sendFile('index.html');
 // })
 app.get("/", (req, res) => {
-  res.sendFile("./index.html");
+  res.sendFile("../../index.html");
   console.log("it is working");
 });
 
